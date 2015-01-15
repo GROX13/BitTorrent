f0a0408adb8fa83ca147b02af2ba0075b7ff9cf9
GROX13 <giorgirokhadze@gmail.com>
2015-01-16 00:20:16 +0400

    Files Organised

---
+++ bt_client/bt_client.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <string.h>
+#include "bencode.h"
+#include <sys/stat.h>
+#include "setup.h"
+
+int main(int argc, char *argv[]) {
+    bencode_t ben;
+
+
+    char filename[1024];
+    get_filename(argc, argv, filename);
+  //  puts(filename);
+    
+    char * file; 
+    long long leng;
+    file = read_file(filename, &leng);
+      
+   //puts(file);
+
+
+    bencode_t ben2;
+
+
+    const char *ren;
+
+    int len, ret;
+
+    bencode_init(&ben, file, strlen(file));
+
+    ret = bencode_dict_get_next(&ben, &ben2, &ren, &len);
+    printf("foo %s %i\n", ren, len);
+    bencode_string_value(&ben2, &ren, &len);
+    //printf("bla %s %i\n", ren, len);
+   
+    return 0;
+}
---
+++ bt_client/bt_library.c
@@ -0,0 +1,1 @@
+bt_library.c
