63fbc0362505f845655ddd5315dbbaa97d8f9da4
AnnaGorozia <ana.gorozia@gmail.com>
2015-01-15 21:03:14 +0400

    reading file finished

--- bt_client.c
+++ bt_client.c
@@ -3,30 +3,6 @@
 #include "bencode.h"
 #include <sys/stat.h>
 #include "setup.h"
-char * read_file(char * file, long long *len){
-  struct stat st;
-  char *ret = NULL;
-  FILE *fp;
-  
-  if (stat(file, &st)){
-    return ret;
-  }
-  *len = st.st_size;
-
-  fp = fopen(file, "r");
-  if (!fp)
-    return ret;
-
-  ret = malloc(*len);
-  if (!ret)
-    return NULL;
-  
-  fread(ret, 1, *len, fp);
-  
-  fclose(fp);
-  
-  return ret;
-}
 
 int main(int argc, char *argv[]) {
     bencode_t ben;
@@ -45,14 +21,14 @@ int main(int argc, char *argv[]) {
     bencode_string_value(&ben, &ren, &len);
     puts(ren);
 
-    char * filename;
+    char filename[1024];
     get_filename(argc, argv, filename);
     puts(filename);
     
-    /*char * file; 
+    char * file; 
     long long leng;
     file = read_file(filename, &leng);
       
-    puts(file);*/
+    puts(file);
     return 0;
 }
