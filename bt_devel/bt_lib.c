46661476e66e86be8e2757d311193257967b9b31
GROX13 <giorgirokhadze@gmail.com>
2015-01-30 14:19:14 +0400

    Updated

--- bt_devel/bt_lib.c
+++ bt_devel/bt_lib.c
@@ -88,6 +88,27 @@ int contact_tracker(bt_args_t *bt_args) {
 
     puts(new_announce);
 
+    CURL *curl;
+    CURLcode res;
+
+    curl = curl_easy_init();
+    if(curl) {
+        curl_easy_setopt(curl, CURLOPT_URL, announce);
+        /* example.com is redirected, so we tell libcurl to follow redirection */
+        curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+
+        /* Perform the request, res will get the return code */
+        res = curl_easy_perform(curl);
+        /* Check for errors */
+        if(res != CURLE_OK)
+            fprintf(stderr, "curl_easy_perform() failed: %s\n",
+                    curl_easy_strerror(res));
+
+        /* always cleanup */
+        curl_easy_cleanup(curl);
+    }
+
+
    /* struct hostent *he;
     struct in_addr **addr_list;
 
