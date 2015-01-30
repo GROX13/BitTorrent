033733f7537755c8a0a57043f290d12fd6096dd0
AnnaGorozia <ana.gorozia@gmail.com>
2015-01-30 21:13:59 +0400

    track

--- bt_devel/bt_lib.c
+++ bt_devel/bt_lib.c
@@ -57,6 +57,64 @@ void reverse(char *x, int begin, int end) {
     reverse(x, ++begin, --end);
 }
 
+struct my_string{
+	char* memory;
+	size_t size;
+};
+
+static size_t my_string_append(void *contents, size_t size, size_t nmemb, void *userp)
+{
+  size_t realsize = size * nmemb;
+  struct my_string *mem = (struct my_string *)userp;
+ 
+  mem->memory = realloc(mem->memory, mem->size + realsize + 1);
+ 
+  memcpy(&(mem->memory[mem->size]), contents, realsize);
+  mem->size += realsize;
+  mem->memory[mem->size] = 0;
+ 
+  return realsize;
+}
+
+char* send_http_request(char* url){
+	CURL *curl_handle;
+	CURLcode res;
+	struct my_string header;
+	struct my_string body;
+	header.memory = malloc(1);
+	header.size = 0;
+	body.memory = malloc(1);
+	body.size = 0;
+	/* init the curl session */ 
+	curl_handle = curl_easy_init();
+	// curl_easy_setopt(curl_handle, CURLOPT_POSTFIELDS, args);
+	curl_easy_setopt(curl_handle, CURLOPT_HTTPGET, 1);
+	/* specify URL to get */ 
+	curl_easy_setopt(curl_handle, CURLOPT_URL, url);
+	/* send all data to this function  */ 
+	curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION, my_string_append);
+	/* we pass our 'chunk' struct to the callback function */ 
+	curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, (void *)&body);
+	curl_easy_setopt(curl_handle, CURLOPT_HEADERDATA, (void *)&header);
+	/* some servers don't like requests that are made without a user-agent
+	 field, so we provide one */ 
+	curl_easy_setopt(curl_handle, CURLOPT_USERAGENT, "libcurl-agent/1.0");
+	/* get it! */ 
+	res = curl_easy_perform(curl_handle);
+	/* check for errors */ 
+	if(res != CURLE_OK) {
+		fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+		return NULL;
+	}else{
+		if(memcmp(body.memory, "d8:", 3) != 0){
+			printf("error\n%s\n", body.memory);
+			return NULL;
+		}else{
+			return body.memory;
+		}
+	}
+}
+
 int contact_tracker(bt_args_t *bt_args) {
     printf("Starting contact tracker:\n");
     printf("Announce: %s\n", bt_args->bt_info->announce);
@@ -68,7 +126,6 @@ int contact_tracker(bt_args_t *bt_args) {
     char *file;
     long long leng;
     file = read_file(bt_args->torrent_file, &leng);
-
     char *hashed_info = malloc(21);
     int len = (int) strlen(strstr(strstr(file, "info"), "d"));
     SHA1((unsigned char const *) strstr(strstr(file, "info"), "d"), (size_t) len, (unsigned char *) hashed_info);
@@ -77,16 +134,17 @@ int contact_tracker(bt_args_t *bt_args) {
 	
 	char *request_to_send;
     request_to_send = malloc(100);
-
+	
 	//aq unda iyos: Port number this peer is listening on. 
 	//Common behavior is for a downloader to try to listen on 
 	//port 6881 and if that port is taken try 6882, then 6883, etc. and give up after 6889.
-	char* port = "6969";
-    sprintf(request_to_send, "%s/announce?info_hash=%s&peer_id=%s&port=%s"
+	int port = INIT_PORT;
+    sprintf(request_to_send, "%s/announce?info_hash=%s&peer_id=%s&port=%i"
                     "&downloaded=0&left=0&event=started", bt_args->bt_info->announce,
             url_encode(hashed_info), url_encode(generate_peer_id()), port);
     
 	printf("Request URL for tracker: %s\n", request_to_send);
+	send_http_request(request_to_send);
     return 0;	
 }
 
