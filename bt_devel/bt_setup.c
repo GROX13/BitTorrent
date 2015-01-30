20db32309d5a704bc03d4c251cb657868cf47e6c
GROX13 <giorgirokhadze@gmail.com>
2015-01-30 15:29:02 +0400

    Updated

--- bt_devel/bt_setup.c
+++ bt_devel/bt_setup.c
@@ -238,30 +238,23 @@ char *generate_peer_id() {
     time_t current_time;
     char *c_time_string;
 
-    /* Obtain current time as seconds elapsed since the Epoch. */
     current_time = time(NULL);
 
-    if (current_time == ((time_t) -1)) {
+    if (current_time == ((time_t) -1))
         (void) fprintf(stderr, "Failure to compute the current time.");
-    }
 
-    /* Convert to local time format. */
     c_time_string = ctime(&current_time);
 
-    if (c_time_string == NULL) {
+    if (c_time_string == NULL)
         (void) fprintf(stderr, "Failure to convert the current time.");
-
-    }
 
     _remove_char(c_time_string, ' ');
     _remove_char(c_time_string, ':');
-    size_t a = strlen(c_time_string);
-    /* Print to stdout. */
-    (void) printf("Current time is %s", c_time_string);
+
     char result[21] = CONS_RAND;
-    printf("%s", result);
+
     memcpy(result, c_time_string, strlen(c_time_string) - 1);
-    a = strlen(result);
+
     return result;
 
 }
